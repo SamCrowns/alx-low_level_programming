99c5742e6c9f621f8fa400044d3ccad0b2c0bf82
Adedoyin Samson <adedoyinsamson080@gmail.com>
2022-09-08 13:13:36 -0700

    Tasks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include<stdio.h>
+/**
+     * main - Entry point
+ *
+     *
+ *
+ *    Return: always 0     */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puziiiiiiiiiii");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
 
 }
