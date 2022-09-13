985e81b2a2179b0b515579e23e35bee7055e4964
Adedoyin Samson <adedoyinsamson080@gmail.com>
2022-09-13 14:47:14 -0700

    Yasks

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - function that compute the absolute value of an integers
+ *
+ * Return: the absolute value of n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_last_digit - function that prints the last digit of number
+ *
+ * Return: the value of the last digit of n
+ */
+int print_last_digit(int n)
+{
+	int d;
+	if (n < 0)
+	{
+		d = (n % 10) * -1;
+		_putchar(d + '0');
+		return (d);
+	}
+	else
+	{
+		d = n % 10;
+		_putchar(d + '0');
+		return (d);
+	}
+}
