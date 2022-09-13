cc9c8596b7a205470abf8abf96ffa01ce7b8c5ca
Adedoyin Samson <adedoyinsamson080@gmail.com>
2022-09-13 13:17:15 -0700

    Task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,7 +9,7 @@ int main(void)
 	char word[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
 	unsigned int c;
 
-	for (c = 0; c < sizeof(putchar); c++)
+	for (c = 0; c < sizeof(word); c++)
 	{
 		_putchar(word[c]);
 	}
