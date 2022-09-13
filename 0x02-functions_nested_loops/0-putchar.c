1335d023bc4ba5869c413057c3e69aebf8bea78a
Adedoyin Samson <adedoyinsamson080@gmail.com>
2022-09-13 12:16:51 -0700

    Tasks

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include "math.h"
+/**
+ * main - Entry
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char putchar[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+	unsigned int c;
+
+	for (c = 0; c < sizeof(putchar); c++)
+		_putchar(putchar[c]);
+	_putchar('\n');
+	return (0);
+}
