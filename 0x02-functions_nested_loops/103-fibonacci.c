917835573421fd9023b7842543377f61def4498f
Adedoyin Samson <adedoyinsamson080@gmail.com>
2022-09-14 00:07:13 -0700

    Tasks

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * main - entry
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	long int j, k, f, sum;
+
+	j = 1;
+	k = 2;
+	f = 0;
+	sum = 0;
+	while (f <= 4000000)
+	{
+		f = k + j;
+		if (k % 2 == 0)
+			sum += k;
+		j = k;
+		k = f;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
