886c5b61acb09cadff2be15ba548267ed6d8cd95
Adedoyin Samson <adedoyinsamson080@gmail.com>
2022-09-14 09:18:39 -0700

    Tasks

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
  * main - tests function that prints if integer is positive or negative
- *
  * Return: 0
  */
 int main(void)
@@ -10,5 +9,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
+	putchar('\n');
 	return (0);
 }
