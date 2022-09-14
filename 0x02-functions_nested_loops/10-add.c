f02945c1112cbd6001c9917cfa38b199d08cd9d0
Adedoyin Samson <adedoyinsamson080@gmail.com>
2022-09-13 22:36:01 -0700

    Tasks

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - function that adds two integers and returns the result
+ * @a: first argument
+ * @b: second argument
+ * Return: the result of the addition of a and b
+ */
+int add(int a, int b);
+{
+	return (a + b);
+}
