cfa5bdc6c808ed055b9a77b821e93ec8fac57116
Adedoyin Samson <adedoyinsamson080@gmail.com>
2022-09-14 08:38:13 -0700

    Tasks

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
 
 void positive_or_negative(int i);
@@ -7,4 +8,4 @@ int largest_number(int, int, int);
 void print_remaning_days(int, int, int);
 int convert_day(int, int);
 
-#endif /* MAIN_H */
+#endif
