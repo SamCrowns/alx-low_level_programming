2a4b10140ef3cf15b9fb5863b6f8ee088cf96efd
Adedoyin Samson <adedoyinsamson080@gmail.com>
2022-09-15 10:01:31 -0700

    Tasks

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isdigit - Write a function that multiplies two integers
+ * @c: integer 1
+ * Return: 0
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c >= '9')
+		return (1);
+	return (0);
+}
