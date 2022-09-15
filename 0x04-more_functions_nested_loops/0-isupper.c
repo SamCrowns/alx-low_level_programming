83e038cc51dae092a4b71c4293410d2301fc3f07
Adedoyin Samson <adedoyinsamson080@gmail.com>
2022-09-15 09:48:01 -0700

    Tasks

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isupper - Write a function that checks for uppercase character.
+ * @c - The characyer in question
+ * Return: 0
+ */
+int _isupper(int c);
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
+}
