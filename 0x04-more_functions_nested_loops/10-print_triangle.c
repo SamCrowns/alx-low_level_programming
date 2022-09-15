7dba8e91333155b39a017ee9e5e889db9af0941e
Adedoyin Samson <adedoyinsamson080@gmail.com>
2022-09-15 11:59:16 -0700

    Tasks

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -26,5 +26,6 @@ void print_triangle(int size)
 			_putchar ('\n');
 		}
 	}
-	else _putchar('\n');
+	else
+		_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,7 +7,7 @@
 void print_number(int n)
 {
 	unsigned int num;
-	
+
 	if (n < 0)
 	{
 		num = -n;
