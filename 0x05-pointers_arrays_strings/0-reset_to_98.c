da128dee16458324828ea7f29c81008e7d0abb5a
Adedoyin Samson <adedoyinsamson080@gmail.com>
2022-09-19 09:47:31 -0700

    Tasks

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include <main.h>
+/**
+ * reset_to_98 - program to change the number inside address to 98
+ * Return: void
+ * @n: parameter of the function
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
