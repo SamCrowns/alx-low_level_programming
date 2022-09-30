09292d48dd0cabbf6fa33db548d9036348ea2031
Adedoyin Samson <adedoyinsamson080@gmail.com>
2022-09-30 04:56:48 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Function that prints its name
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d", argv[0]);
+	return (0);
+}
