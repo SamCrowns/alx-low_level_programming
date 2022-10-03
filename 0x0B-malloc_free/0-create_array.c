f07c26f7dcb051390cfa0a4b64cb8e80f8b60e3e
Adedoyin Samson <adedoyinsamson080@gmail.com>
2022-10-03 09:24:07 -0700

    Task 1

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * create_array - A function that creates an array of chars,
+ * @size: The size of the array
+ * @c: A character to initialized the array
+ * Return: Apointer to the array or NULL if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	unsigned int i = 0;
+
+	s = malloc(size * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+	if (size == 0)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		s[i] = c;
+	return (s);
+}
