0e559cdda58eab64bb41fa4cbfbd1aadd8a08e50
Adedoyin Samson <adedoyinsamson080@gmail.com>
2022-10-13 09:16:16 -0700

    tasks

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -2,7 +2,7 @@
 #include <stdarg.h>
 #include <stdio.h>
 /**
- * void print_numbers - a function that prints numbers, followed by a new line.
+ * print_numbers - a function that prints numbers, followed by a new line.
  * @separator: the string to be printed between numbers
  * @n: the number of integers passed to the function
  * Return: The required output
